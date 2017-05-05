b4f0bdb9c4eced3383bfe2126c3a18ef9ae5675d
DELEGACION TERRITORIAL VALLADOLID <DtVaAula@jcyl.red>
2017-05-05 13:28:21 +0200

    Gradle

---
+++ 13_HolaMundoGradle/src/test/java/com/ejemplo/PruebaTest.java
@@ -0,0 +1,14 @@
+package com.ejemplo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
